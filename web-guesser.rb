c02727728798830792aee74ba0b2c39afdbad4b1
natamarkova <nataliamarkovabox@gmail.com>
2020-09-22 16:06:07 -0700

    Completed assignment b1-l6

--- web-guesser.rb
+++ web-guesser.rb
@@ -1,26 +1,77 @@
-require "sinatra"
+require 'sinatra'
+
 enable :sessions
 
-get "/" do
-    if !session[:secret]
-        session[:secret] = rand(100)+1
+helpers do
+  def guessed_number
+    session[:number]
+  end
+
+  def secret_number
+    session[:secret]
+  end
+
+  def guess_description
+    if guessed_number > secret_number
+        diff = guessed_number - secret_number
+        if diff <= 10
+            too_what = "close but too high"
+        else diff > 10
+            too_what = "much too high"
+        end
+    elsif guessed_number < secret_number
+        diff = secret_number - guessed_number
+        if diff <= 10
+            too_what = "close but too low"
+        else diff > 10
+            too_what = "much too low"
+        end
     end
-    if !session[:counter]
-        session[:counter] = 0
-    end
-    puts "\n\n\n #{session[:secret]}"
-    if session[:number] == session[:secret]
+
+    puts "Your guess of #{guessed_number} was #{too_what}."
+    session[:message] = "Try Again! Your guess of #{guessed_number} is #{too_what}."
+  end
+end
+
+get '/' do
+    set_secret_number
+    set_counter
+    set_message
+    puts "\n\n\nsession_secret=#{session[:secret]}"
+    erb :form
+end
+
+post "/" do
+    session[:number] = params[:number].to_i if params[:number]
+    session[:counter] += 1
+
+    if guessed_correctly?
+        puts "\n\n\nYOU WIN"
         erb :win
     elsif session[:counter] >= 7
-        erb :lose
+        puts "\n\n\nYOU LOSE"
+        erb :lose  
     else
-        puts "session_number: " + session[:number].to_s if session[:number]
+        guess_description
+        session[:message]
         erb :form
     end
 end
-post "/" do
-    session[:name] = params[:name]
-    session[:number] = params[:number].to_i
-    session[:counter] += 1
-    redirect "/"
+
+private
+
+def guessed_correctly?
+    session[:number] == session[:secret]
+end
+
+def set_secret_number
+    session[:secret] = rand(100)+1
+end
+
+def set_counter
+    session[:counter] = 0
+end
+
+def set_message
+    session[:message]= ""
 end
